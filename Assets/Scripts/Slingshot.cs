c35cd83dbf6fc52b217f21499324362f7d321c0d
Natascha Schulz <natascha-schulz@hotmail.com>
2015-07-17 23:21:31 +0200

    Init

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+	
+	// Fields set in the Unity Inspector pane
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	
+	
+	// Fields set dynamically
+	private GameObject launchPoint;
+	private Vector3 launchPos;
+	private GameObject projectile;
+	private bool aimingMode;
+	
+	void Awake(){
+		//print ("Awake()");
+		Transform launchPointTrans = transform.FindChild("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+		launchPos = launchPointTrans.position;
+	}
+	
+	void OnMouseEnter() {
+		//print ("Enter");
+		launchPoint.SetActive(true);
+	}
+	
+	void OnMouseExit() {
+		//print ("Exit");
+		if(!aimingMode) 
+			launchPoint.SetActive(false);
+	}
+	
+	void OnMouseDown(){
+		
+		GetComponent<AudioSource>().Play();
+		//print ("Down");
+		
+		// Player pressed mouse while over Slingshot
+		aimingMode = true;
+		
+		// Instantiate a projectile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+		
+		// Start it at launch position
+		projectile.transform.position = launchPos;
+		
+		// Set it to kinematic for now
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+	}
+	
+	
+	
+	
+	void Update() {
+		// If the Slingshot is not in aiming mode, don't run this code
+		if(!aimingMode) return;
+		
+		// Get the current mouse position in 2D screen coordinates
+		Vector3 mousePos = Input.mousePosition;
+		// Convert the mouse position to 3D world coordinates
+		mousePos.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos);
+		
+		// Find the delta from launch position to 3D mouse position
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		
+		// Limit mouseDelta to the radius of the Slingshot SphereCollider
+		float maxMagnitude = GetComponent<SphereCollider>().radius;
+		mouseDelta = Vector3.ClampMagnitude(mouseDelta, maxMagnitude);
+		
+		// Now move the projectile to this new position
+		projectile.transform.position = launchPos + mouseDelta;
+		
+		if(Input.GetMouseButtonUp(0)) {
+			// The mouse has been released
+			aimingMode = false;
+			// Fire off the projectile with given velocity
+			projectile.GetComponent<Rigidbody>().isKinematic = false;
+			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+			
+			// Set the Followcam's target to our projectile
+			FollowCam.S.poi = projectile;
+			
+			// Set the reference to the projectile to null as early as possible
+			projectile = null;
+			
+			GameController.ShotFired();
+			GetComponent<AudioSource>().Stop();
+			
+		}
+		
+		
+		
+	}
+}
---
+++ Assets/Scripts/StopNoise.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopNoise : MonoBehaviour {
+
+	private float TimeBeforeNoiseStops;
+	void Start () {
+	
+		var sound = this.GetComponent<AudioSource>();
+		TimeBeforeNoiseStops = sound.clip.length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		TimeBeforeNoiseStops -= Time.deltaTime;
+		if(TimeBeforeNoiseStops <= 0f)
+		   Destroy (this.gameObject);
+
+	}
+}
